46f785d47bf11c81e2e8bf3a04073cb22d9385ac
Ioannis Canellos <iocanel@gmail.com>
2020-08-18 20:58:22 +0300

    feat: added setup script.

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SOURCE_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+echo "Linking emacs.d init files ..."
+ln -s ${SOURCE_PATH}/early-init.el ${HOME}/.emacs.d/early-init.el
+ln -s ${SOURCE_PATH}/init.el ${HOME}/.emacs.d/init.el
