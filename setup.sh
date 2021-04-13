373d3b29c51799331cffdc19e2c8bbb8d0f70858
Ioannis Canellos <iocanel@gmail.com>
2021-04-13 10:43:06 +0300

    feat: add stragith versions

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ SOURCE_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo "Linking emacs.d init files ..."
 ln -s ${SOURCE_PATH}/early-init.el ${HOME}/.emacs.d/early-init.el
 ln -s ${SOURCE_PATH}/init.el ${HOME}/.emacs.d/init.el
+
+# Copy the versions profile
+mkdir -p ${HOME}/.emacs.d/straight/versions
+cp ${HOME}/versions/default.el ${HOME}/.emacs.d/straight/versions/
