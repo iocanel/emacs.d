d145bba842c67634a789010a036aa54539f031a7
Ioannis Canellos <iocanel@gmail.com>
2021-10-29 15:28:45 +0300

    fix: finalize properly compiles config

--- finalize.el
+++ finalize.el
@@ -1,5 +1,23 @@
 ;;: Turn messages back on
 
-(byte-recompile-directory (concat
-                           (file-name-as-directory user-emacs-directory) "straight/build"))
- (setq inhibit-message nil)
+;; Recomplie config if it's not already compiled
+(let ((lock-file (locate-user-emacs-file ".config.lock")))
+  (when (not (file-exists-p lock-file))
+    ;; Compile configuration files
+    (ic/recompile-configuration)
+    ;; Create lockfile
+    (with-temp-buffer (write-file lock-file))))
+
+(defun ic/recompile-configuration()
+  "Recompile configurration."
+  (interactive)
+    (let ((config-dir "/home/iocanel/workspace/src/github.com/iocanel/emacs.d/"))
+    (mapc (lambda (e)
+            (let ((elisp-file (concat config-dir e)))
+              (message "Compiling %s." elisp-file)
+              (condition-case nil
+                  (native-compile elisp-file)
+                (error nil))))
+          (seq-filter (lambda (f) (string-match "\.el$" f)) (directory-files config-dir)))))
+
+(setq inhibit-message nil)
