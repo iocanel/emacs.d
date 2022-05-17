56910eb27e7d93810ae61213a1013fa463c8d184
Ioannis Canellos <iocanel@gmail.com>
2022-05-17 15:59:25 +0300

    chore: move eshell function under config section

--- +shell.el
+++ +shell.el
@@ -10,67 +10,64 @@
         eshell-history-size 100000
         eshell-banner-message ""
         eshell-path-env (getenv "PATH"))
-  :bind (:map eshell-command-mode-map
-              ("M-w" . 'iocanel/eshell-open-eww)
-              ("M-x" . 'iocanel/eshell-open-xwidget-webkit))
-  :hook ((eshell-pre-command . (lambda ()
-                                  (iocanel/eshell-refresh-compilation-mode)
-                                  (setq eshell-path-env (getenv "PATH"))))
-         (eshell-mode . (lambda()
-                               (setq eshell-path-env (getenv "PATH"))
-                               (eshell/alias "mvn" "~/bin/mvnnotify $*")
-                               (eshell/alias "cls" "iocanel/eshell-clear")
-                               (eshell/alias "clear" "iocanel/eshell-clear")
-                               (eshell/alias "d" "dired $1")
-                               (eshell/alias "e" "find-file $1")
-                               (eshell/alias "ee" "find-file-other-window $1")
-                               (eshell/alias "emacs" "find-file-other-window $1")
-                               (eshell/alias "ff" "find-file $1")
-                               (eshell/alias "gd" "magit-diff-unstaged")
-                               (eshell/alias "gds" "magit-diff-staged")
-                               (eshell/alias "ll" "ls -AlohG --color=always $*")
-                               (eshell/alias "ls" "TERM=ansi ls --color=always $*")
-                               (eshell/alias "vi" "find-file-silent $1")
-                               (eshell/alias "kc" "kubectl $*")))))
 
-;;;###autoload
-(defun iocanel/eshell-clear ()
-  "Clear the eshell buffer."
-  (interactive)
-  (let ((inhibit-read-only t))
-    (erase-buffer)))
-;;    (eshell-send-input nil nil nil)))
+  (defun ic/eshell-clear ()
+    "Clear the eshell buffer."
+    (interactive)
+    (let ((inhibit-read-only t))
+      (erase-buffer)))
 
-;;;###autoload
-(defun iocanel/eshell-refresh-compilation-mode () 
-  "Re-enable compilation mode"
-  (interactive)
-  ;; compilation shell mode doesn't work properly so it needs a nudge every now and then
-  (compilation-shell-minor-mode -1)
-  (compilation-shell-minor-mode 1))
+  (defun ic/eshell-refresh-compilation-mode () 
+    "Re-enable compilation mode"
+    (interactive)
+    ;; compilation shell mode doesn't work properly so it needs a nudge every now and then
+    (compilation-shell-minor-mode -1)
+    (compilation-shell-minor-mode 1))
 
 
-;;;###autoload
-(defun iocanel/eshell-open-eww ()
-  (interactive)
-  "Start eww from the current ehsell buffer."
-  (let* ((port (iocanel/find-web-port))
-         (url (format "http://localhost:%s" port)))
-    (eww url)))
+  (defun ic/eshell-open-eww ()
+    (interactive)
+    "Start eww from the current ehsell buffer."
+    (let* ((port (ic/find-web-port))
+           (url (format "http://localhost:%s" port)))
+      (eww url)))
 
-;;;###autoload
-(defun iocanel/eshell-open-xwidget-webkit ()
-  (interactive)
-  "Start eww from the current ehsell buffer."
-  (let* ((port (iocanel/find-web-port))
-         (url (format "http://localhost:%s" port)))
+  (defun ic/eshell-open-xwidget-webkit ()
+    (interactive)
+    "Start eww from the current ehsell buffer."
+    (let* ((port (ic/find-web-port))
+           (url (format "http://localhost:%s" port)))
       (xwidget-webkit-browse-url url)))
 
-;;;###autoload
-(defun iocanel/find-web-port ()
-  "Find the web port in the current buffer, or return 8080 if none is found."
-  (save-excursion
-    (goto-char (point-max))
-    (if (re-search-backward "port.* \\([0-9]+\\)" nil t)
-        (match-string 1)
-      8080)))
+  (defun ic/find-web-port ()
+    "Find the web port in the current buffer, or return 8080 if none is found."
+    (save-excursion
+      (goto-char (point-max))
+      (if (re-search-backward "port.* \\([0-9]+\\)" nil t)
+          (match-string 1)
+        8080)))
+
+
+  :bind (:map eshell-command-mode-map
+              ("M-w" . 'ic/eshell-open-eww)
+              ("M-x" . 'ic/eshell-open-xwidget-webkit))
+  :hook ((eshell-pre-command . (lambda ()
+                                 (ic/eshell-refresh-compilation-mode)
+                                 (setq eshell-path-env (getenv "PATH"))))
+         (eshell-mode . (lambda()
+                          (setq eshell-path-env (getenv "PATH"))
+                          (eshell/alias "mvn" "~/bin/mvnnotify $*")
+                          (eshell/alias "cls" "ic/eshell-clear")
+                          (eshell/alias "clear" "ic/eshell-clear")
+                          (eshell/alias "d" "dired $1")
+                          (eshell/alias "e" "find-file $1")
+                          (eshell/alias "ee" "find-file-other-window $1")
+                          (eshell/alias "emacs" "find-file-other-window $1")
+                          (eshell/alias "ff" "find-file $1")
+                          (eshell/alias "gd" "magit-diff-unstaged")
+                          (eshell/alias "gds" "magit-diff-staged")
+                          (eshell/alias "ll" "ls -AlohG --color=always $*")
+                          (eshell/alias "ls" "TERM=ansi ls --color=always $*")
+                          (eshell/alias "vi" "find-file-silent $1")
+                          (eshell/alias "kc" "kubectl $*")))))
+
