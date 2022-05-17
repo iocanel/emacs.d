a733fd927713b765905e7123c05354404740d7e3
Ioannis Canellos <iocanel@gmail.com>
2022-05-17 15:55:45 +0300

    fix: real-auto-save configuration

--- +autosave.el
+++ +autosave.el
@@ -1,16 +1,16 @@
 (use-package real-auto-save
-  :defer t
+  :ensure t ;; Won't work if we defer.
   :config
   (progn 
-     (setq real-auto-save-interval 600
-        backup-directory-alist `(("." . "~/.emacs.d/backups/"))
-        auto-save-file-name-transforms `((".*" "~/.emacs.d/autosaves/" t))
-        delete-old-versions t
-        kept-new-versions 6
-        kept-old-versions 2
-        version-control t
-        create-lockfiles nil)
-     (global-auto-revert-mode 1))
+    (setq real-auto-save-interval 10
+          backup-directory-alist `(("." . ,(concat user-emacs-directory "backups/")))
+          auto-save-file-name-transforms `((".*" ,(concat user-emacs-directory "autosaves/") t))
+          delete-old-versions t
+          kept-new-versions 6
+          kept-old-versions 2
+          version-control t
+          create-lockfiles nil)
+    (global-auto-revert-mode 1))
   :hook ((text-mode . real-auto-save-mode)
          (prog-mode . real-auto-save-mode)
          (snippet-mode . (lambda () (real-auto-save-mode -1)))))
