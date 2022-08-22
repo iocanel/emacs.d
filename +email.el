b8c2a44570a4d1d62dd30bd8c19c3a47af98503b
Ioannis Canellos <iocanel@gmail.com>
2022-08-22 16:41:38 +0300

    refactor: move all email config and code under use-package config

--- +email.el
+++ +email.el
@@ -5,13 +5,18 @@
 (use-package mu4e
   :defer t
   :straight (mu4e :type git :host github :repo "djcb/mu" :branch "1.6.9-signed" :files (:defaults "mu4e/*.el"))
-  :bind (("C-c a m m" . mu4e)
-         ("C-c a m n" . mu4e-compose-new)
-         ("C-c a m u" . ic/mu4e-view-unread))
+  :defer t
+  :commands (mu4e mu4e-compose-new ic/mu4e-view-unread ic/mu4e-force-next-unread ic/mu4e-mark-thread-as-unread)
   :config
   (setq user-mail-address "iocanel@gmail.com"
         user-full-name "Ioannis Canellos"
         mu4e-maildir "~/.mail"
+
+        ;; Having Error: 102: failed to move message
+        ;; The following block of config is suggested by https://github.com/djcb/mu/issues/2053
+        mu4e-index-lazy-check nil
+        mu4e-change-filenames-when-moving t
+
         mu4e-compose-context-policy 'ask
         mu4e-context-policy 'ask
         mu4e-contexts
@@ -64,6 +69,7 @@
                      (message-sendmail-f-is-evil       . t)
                      (mu4e-sent-messages-behavior      . delete)
                      (mu4e-compose-signature           .  t)))))
+
   (set-face-attribute 'mu4e-replied-face nil :inherit 'link :underline nil)
   (set-face-attribute 'mu4e-trashed-face nil :foreground "#555555")
 
@@ -81,15 +87,99 @@
   ;; enable inline images
   (setq mu4e-view-show-images t)
   (setq org-mu4e-convert-to-html t)
+
+  ;; Mu4e Bookmarks
+  (setq mu4e-bookmarks
+        '(
+          ("date:2d..now AND flag:unread AND NOT flag:trashed AND not flag:list AND date:30d..now AND (to:iocanel or ikanello)" "Must read" ?i)
+
+          ("NOT flag:trashed AND NOT maildir:\"/Archived\"" "Messages (all)" ?U)
+          ("flag:unread AND NOT flag:trashed AND NOT maildir:\"/Archived\"" "Messages (unread)" ?u)
+
+          ("not flag:list AND date:30d..now AND (to:iocanel or ikanello)" "Personal (all)" ?P)
+          ("flag:unread AND not flag:list AND date:30d..now AND (to:iocanel or ikanello)" "Personal (unread)" ?p)
+
+          ;; Github
+          ("from:github AND AND NOT flag:trashed AND NOT maildir:\"/Archived\"" "Github (all)" ?G)
+          ("flag:unread AND from:github AND AND NOT flag:trashed AND NOT maildir:\"/Archived\"" "Github (unread)" ?g)
+          ("flag:unread AND from:notifications@github.com AND AND NOT flag:trashed AND cc:review_requested AND NOT maildir:\"/Archived\"" "Github (review)" ?r)
+          ("flag:unread AND from:notifications@github.com AND AND NOT flag:trashed AND cc:mention AND NOT maildir:\"/Archived\"" "Github (mentions)" ?m)
+
+          ;; Events
+          ("mime:text/calendar" "Events (all)" ?E)
+          ("flat:unread AND mime:text/calendar" "Events (unread)" ?e)
+
+          ;; Period
+          ("date:today" "Today's messages" ?t)
+          ("date:7d..now" "Last 7 days" ?w)))
+
+
+  ;; Sending Emails
+  (setq message-send-mail-function 'message-send-mail-with-sendmail)
+  (setq sendmail-program "msmtp")
+  (setq message-sendmail-extra-arguments '("-C" "/home/iocanel/.config/msmtp/config" "--read-envelope-from"))
+  (setq message-sendmail-f-is-evil 't)
+  (setq message-kill-buffer-on-exit t)
+  (setq doom-modeline-mu4e t)
+
   (add-to-list 'mu4e-view-actions '("ViewInBrowser" . mu4e-action-view-in-browser) t)
 
-  (require 'mu4e-headers)
-  (require 'mu4e-view)
   ;; Add custom actions for our capture templates
   (add-to-list 'mu4e-headers-actions '("follow up" . ic/mu4e-capture-follow-up) t)
   (add-to-list 'mu4e-view-actions '("follow up" . ic/mu4e-capture-follow-up) t)
   (add-to-list 'mu4e-headers-actions '("read later" . ic/mu4e-capture-read-later) t)
   (add-to-list 'mu4e-view-actions '("read later" . ic/mu4e-capture-read-later) t)
+
+  ;;
+  ;; Functions
+  ;;
+
+  (defun ic/mu4e-force-next-unread()
+    "View next unread closing the current message if stuck in loading."
+    (interactive)
+    (if (eq 'mu4e-loading-mode major-mode)
+        (progn
+          (select-window (get-buffer-window "*mu4e-headers*"))
+          (delete-other-windows)
+          (mu4e-view-headers-next-unread)
+          (mu4e-headers-view-message))
+      (mu4e-view-headers-next-unread)))
+
+  (defun ic/mu4e-mark-thread-as-read()
+    "Skip all messages from the current thread."
+    (interactive)
+    (save-excursion
+      (select-window (get-buffer-window "*mu4e-headers*"))
+      (recenter)
+      (mu4e-headers-mark-thread t '(read))))
+
+  (defun ic/mu4e-view-unread()
+    "Open my unread messages."
+    (interactive)
+    (require 'mu4e)
+    (mu4e-headers-search
+     (mu4e-bookmark-query (car (remove-if-not (lambda (s) (equal (mu4e-bookmark-name s) "Unread messages")) (mu4e-bookmarks))))))
+
+
+  ;; Capturing, source: https://github.com/daviwil/emacs-from-scratch/blob/master/show-notes/Emacs-Mail-05.org#adding-custom-actions-for-quick-capturing
+  (defun ic/mu4e-capture-follow-up (&optional msg)
+    "Create a follow up todo item."
+    (interactive)
+    (call-interactively 'org-store-link)
+    (org-capture nil "ef"))
+
+  (defun ic/mu4e-capture-read-later (&optional msg)
+    "Create a read later todo item."
+    (interactive)
+    (call-interactively 'org-store-link)
+    (org-capture nil "er"))
+
+
+  (defun ic/mu4e-get-incoming-count ()
+    "Count the number of unread messages."
+    (let* ((query "flag:unread AND NOT flag:trashed AND NOT maildir:\"/Archived\"")
+           (command (format "mu find '%s' 2>/dev/null | wc -l" query)))
+      (string-trim (shell-command-to-string command))))
 
   ;;
   ;; Advices
@@ -133,6 +223,16 @@
   (ad-activate 'mu4e-view-headers-next-unread)
   (ad-activate 'mu4e-view-headers-prev-unread)
 
+  ;; Mu4e cusotmization
+  (defalias 'org-mail 'org-mu4e-compose-org-mode)
+                                        ; use imagemagick, if available
+  (when (fboundp 'imagemagick-register-types)
+  (imagemagick-register-types))
+
+  (defun no-auto-fill ()
+    "Turn off auto-fill-mode."
+    (auto-fill-mode -1))
+
   :hook ((mu4e-view-mode . (lambda () (mu4e-mark-region-code) (smiley-buffer)))
          (mu4e-compose-mode . (lambda ()
                                 (no-auto-fill)
@@ -141,111 +241,20 @@
                                 (visual-fill-column-mode)
                                 (setq visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow))
                                 (visual-line-mode))))
-  :bind (:map mu4e-view-mode-map
-              ("C-<tab>" . mu4e-view-headers-next-unread)
-              ("C-t" . ic/mu4e-mark-thread-as-read)
-              ("C-r" . ic/mu4e-capture-read-later)
-              ("C-f" . ic/mu4e-capture-follow-up)
-              :map mu4e-headers-mode-map
-              ("C-<tab>" . mu4e-headers-next-unread)
-              ("C-t" . ic/mu4e-mark-thread-as-read)
-              ("C-r" . ic/mu4e-capture-read-later)
-              ("C-f" . ic/mu4e-capture-follow-up)))
-
-
-                                        ;(require 'evil-mu4e)
-
-;; Mu4e cusotmization
-(setq doom-modeline-mu4e t)
-
-
-(defalias 'org-mail 'org-mu4e-compose-org-mode)
-
-                                        ; use imagemagick, if available
-(when (fboundp 'imagemagick-register-types)
-  (imagemagick-register-types))
-
-(defun no-auto-fill ()
-  "Turn off auto-fill-mode."
-  (auto-fill-mode -1))
-
-;; Mu4e Bookmarks
-(setq mu4e-bookmarks
-      '(
-        ("date:2d..now AND flag:unread AND NOT flag:trashed AND not flag:list AND date:30d..now AND (to:iocanel or ikanello)" "Must read" ?i)
-
-        ("NOT flag:trashed AND NOT maildir:\"/Archived\"" "Messages (all)" ?U)
-        ("flag:unread AND NOT flag:trashed AND NOT maildir:\"/Archived\"" "Messages (unread)" ?u)
-
-        ("not flag:list AND date:30d..now AND (to:iocanel or ikanello)" "Personal (all)" ?P)
-        ("flag:unread AND not flag:list AND date:30d..now AND (to:iocanel or ikanello)" "Personal (unread)" ?p)
-
-        ;; Github
-        ("from:github AND AND NOT flag:trashed AND NOT maildir:\"/Archived\"" "Github (all)" ?G)
-        ("flag:unread AND from:github AND AND NOT flag:trashed AND NOT maildir:\"/Archived\"" "Github (unread)" ?g)
-        ("flag:unread AND from:notifications@github.com AND AND NOT flag:trashed AND cc:review_requested AND NOT maildir:\"/Archived\"" "Github (review)" ?r)
-        ("flag:unread AND from:notifications@github.com AND AND NOT flag:trashed AND cc:mention AND NOT maildir:\"/Archived\"" "Github (mentions)" ?m)
-
-        ;; Events
-        ("mime:text/calendar" "Events (all)" ?E)
-        ("flat:unread AND mime:text/calendar" "Events (unread)" ?e)
-
-        ;; Period
-        ("date:today" "Today's messages" ?t)
-        ("date:7d..now" "Last 7 days" ?w)))
-        
-
-;; Sending Emails
-(setq message-send-mail-function 'message-send-mail-with-sendmail)
-(setq sendmail-program "msmtp")
-(setq message-sendmail-extra-arguments '("-C" "/home/iocanel/.config/msmtp/config" "--read-envelope-from"))
-(setq message-sendmail-f-is-evil 't)
-(setq message-kill-buffer-on-exit t)
-
-;;
-;; Functions
-;;
-
-;;;###autoload
-(defun ic/mu4e-mark-thread-as-read()
-  "Skip all messages from the current thread."
-  (interactive)
-  (save-excursion
-    (select-window (get-buffer-window "*mu4e-headers*"))
-    (recenter)
-    (mu4e-headers-mark-thread t '(read))))
-
-;;;###autoload
-(defun ic/mu4e-view-unread()
-  "Open my unread messages."
-  (interactive)
-  (require 'mu4e)
-  (mu4e-headers-search
-   (mu4e-bookmark-query (car (remove-if-not (lambda (s) (equal (mu4e-bookmark-name s) "Unread messages")) (mu4e-bookmarks))))))
-
-;;;###autoload
-(defun ic/mu4e-get-incoming-count ()
-  "Count the number of unread messages."
-  (let* ((query "flag:unread AND NOT flag:trashed AND NOT maildir:\"/Archived\"")
-         (command (format "mu find '%s' 2>/dev/null | wc -l" query)))
-    (string-trim (shell-command-to-string command))))
-
-(define-key evil-normal-state-map (kbd "SPC a m u") #'ic/mu4e-view-unread)
-(global-set-key (kbd "C-c a m u") #'ic/mu4e-view-unread)
-
-
-;; Capturing, source: https://github.com/daviwil/emacs-from-scratch/blob/master/show-notes/Emacs-Mail-05.org#adding-custom-actions-for-quick-capturing
-
-;;;###autoload
-(defun ic/mu4e-capture-follow-up (&optional msg)
-  "Create a follow up todo item."
-  (interactive)
-  (call-interactively 'org-store-link)
-  (org-capture nil "ef"))
-
-;;;###autoload
-(defun ic/mu4e-capture-read-later (&optional msg)
-  "Create a read later todo item."
-  (interactive)
-  (call-interactively 'org-store-link)
-  (org-capture nil "er"))
+  :bind (("C-c a m m" . mu4e)
+         ("C-c a m n" . mu4e-compose-new)
+         ("C-c a m u" . ic/mu4e-view-unread)
+         :map evil-normal-state-map
+         ("SPC a m u" . ic/mu4e-view-unread)
+         :map mu4e-view-mode-map
+         ("C-<tab>" . mu4e-view-headers-next-unread)
+         ("C-t" . ic/mu4e-mark-thread-as-read)
+         ("C-r" . ic/mu4e-capture-read-later)
+         ("C-f" . ic/mu4e-capture-follow-up)
+         :map mu4e-loading-mode-map
+         ("C-<tab>" . ic/mu4e-force-next-unread)
+         :map mu4e-headers-mode-map
+         ("C-<tab>" . mu4e-headers-next-unread)
+         ("C-t" . ic/mu4e-mark-thread-as-read)
+         ("C-r" . ic/mu4e-capture-read-later)
+         ("C-f" . ic/mu4e-capture-follow-up)))
