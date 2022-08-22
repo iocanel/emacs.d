f6c26a347fd5e21336e3c21889ddf15420e85833
Ioannis Canellos <iocanel@gmail.com>
2022-08-22 15:00:02 +0300

    refactor: configure eyuml and flowchart-js babel integration

--- +uml.el
+++ +uml.el
@@ -4,7 +4,8 @@
 ;; PlantUML
 ;;
 (use-package plantuml-mode
-  :commands plantuml-download-jar
+  :after org
+  :commands (plantuml-mode plantuml-download-jar)
   :init
   (add-to-list 'org-src-lang-modes '("plantuml" . plantuml)
                (setq plantuml-jar-path (concat user-emacs-directory "plantuml/" "plantuml.jar")
@@ -19,54 +20,56 @@
 ;; yuml
 ;;
 (use-package eyuml
-  :init (add-to-list 'org-src-lang-modes '("yuml" . yuml)))
+  :after org
+  :init
+  (add-to-list 'org-src-lang-modes '("yuml" . yuml))
+  (add-to-list 'org-src-lang-modes '("flowchart-js" . flowchart-js))
+  :commands (org-babel-execute:yuml)
+  :config
+  ;;
+  ;; Flowchart.js
+  ;;
+  (defun org-babel-execute:flowchart-js (body params)
+    "Execute a block of flowchartjs code with org-babel."
+    (let* ((in-file (org-babel-temp-file "" ".flowchart-js"))
+           (out-file (or (cdr (assq :file params))
+                         (error "flowchart-js requires a \":file\" header argument")))
+           (cmd (format "diagrams flowchart %s %s" in-file out-file))
+           (verbosity (or (cdr (assq :verbosity params)) 0)))
+      (with-temp-buffer
+        (insert body)
+        (goto-char (point-min))
+        (write-region nil nil in-file))
+      (shell-command cmd)
+      nil))
 
-(defun org-babel-execute:yuml (body params)
-  "Execute a block of yuml code with org-babel."
-  (let ((in-file (org-babel-temp-file "" ".yuml"))
-        (type (or (cdr (assq :type params))
-		      (error "yuml requires a \":type\" header argument")))
-        (out-file (or (cdr (assq :file params))
-		      (error "yuml requires a \":file\" header argument")))
-        (verbosity (or (cdr (assq :verbosity params)) 0)))
-    (with-temp-buffer
-      (insert body)
-      (goto-char (point-min))
-      (while (search-forward "\n" nil t) (replace-match "," nil t))
-      (write-region nil nil in-file)
-      (message (buffer-substring (point-min) (point-max)))
-      (eyuml-create-document type out-file))
-    (format "[[file:%s]]" out-file)))
+  (defun org-babel-execute:yuml (body params)
+    "Execute a block of yuml code with org-babel."
+    (let ((in-file (org-babel-temp-file "" ".yuml"))
+          (type (or (cdr (assq :type params))
+                    (error "yuml requires a \":type\" header argument")))
+          (out-file (or (cdr (assq :file params))
+                        (error "yuml requires a \":file\" header argument")))
+          (verbosity (or (cdr (assq :verbosity params)) 0)))
+      (with-temp-buffer
+        (insert body)
+        (goto-char (point-min))
+        (while (search-forward "\n" nil t) (replace-match "," nil t))
+        (write-region nil nil in-file)
+        (message (buffer-substring (point-min) (point-max)))
+        (eyuml-create-document type out-file))
+      (format "[[file:%s]]" out-file)))
 
-(defun eyuml-create-document (type &optional out-file)
-  "Fetch remote document, TYPE could be class,usecase or activity."
-  (let ((out-file (or out-file (eyuml-create-file-name))))
-    (request (eyuml-create-url type)
-      :parser 'buffer-string
-      :success (cl-function
-                (lambda (&key data &allow-other-keys)
-                  (when data
-                    (with-temp-buffer 
-                      (set-buffer-file-coding-system 'raw-text)
-                      (insert data)
-                      (write-region nil nil out-file))))))))
-
-;;
-;; Flowchart.js
-;;
-
-(add-to-list 'org-src-lang-modes '("flowchart-js" . flowchart-js))
-(defun org-babel-execute:flowchart-js (body params)
-  "Execute a block of flowchartjs code with org-babel."
-  (let* ((in-file (org-babel-temp-file "" ".flowchart-js"))
-         (out-file (or (cdr (assq :file params))
-		      (error "flowchart-js requires a \":file\" header argument")))
-         (cmd (format "diagrams flowchart %s %s" in-file out-file))
-        (verbosity (or (cdr (assq :verbosity params)) 0)))
-    (with-temp-buffer
-      (insert body)
-      (goto-char (point-min))
-      (write-region nil nil in-file))
-    (shell-command cmd)
-    nil))
+  (defun eyuml-create-document (type &optional out-file)
+    "Fetch remote document, TYPE could be class,usecase or activity."
+    (let ((out-file (or out-file (eyuml-create-file-name))))
+      (request (eyuml-create-url type)
+        :parser 'buffer-string
+        :success (cl-function
+                  (lambda (&key data &allow-other-keys)
+                    (when data
+                      (with-temp-buffer 
+                        (set-buffer-file-coding-system 'raw-text)
+                        (insert data)
+                        (write-region nil nil out-file)))))))))
 
