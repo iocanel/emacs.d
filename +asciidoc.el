af57297168fe67a9b65e33a0f4a13b1490d15495
Ioannis Canellos <iocanel@gmail.com>
2022-08-22 12:10:12 +0300

    refactor: adoc-mode is lazily loaded for .adoc files

--- +asciidoc.el
+++ +asciidoc.el
@@ -1,13 +1,17 @@
 ;;; +asciidoc.el --- Asciidoc -*- lexical-binding: t -*-
 
-(use-package adoc-mode)
+(use-package adoc-mode
+  :defer t
+  :mode "\\.adoc\\'"
+  :commands (adoc-mode))
+
 (use-package ox-asciidoc
   :defer t
   :commands (org-asciidoc-export-to-asciidoc org-asciidoc-export-as-asciidoc))
 
 (use-package livemarkup
   :straight (livemarkup :host github :repo "dawsers/emacs-livemarkup")
-  :defer t
+  :after adoc-mode
   :commands (livemarkup-track-asciidoc livemarkup-track-org livemark-untrack)
   :config
   (setq livemarkup-output-directory "/tmp"
