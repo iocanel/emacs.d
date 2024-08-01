8420bc0f92a652a0c6b36afa6c98e35bd61aad76
Ioannis Canellos <iocanel@gmail.com>
2024-08-01 10:17:31 +0300

    chore: misc changes

--- init.el
+++ init.el
@@ -2,3 +2,16 @@
  (expand-file-name
   "readme.org"
   user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-ditaa-jar-path
+   "/nix/store/0g51hvz2286c7skks1hcykgr3s6fx2qq-ditaa-0.11.0/lib/ditaa.jar"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
