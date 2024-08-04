9d7eaccf110c296685bc642a385dfbdada4febb6
Ioannis Canellos <iocanel@gmail.com>
2024-08-04 12:56:04 +0300

    chore: bump init.el

--- init.el
+++ init.el
@@ -7,6 +7,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4825b816a58680d1da5665f8776234d4aefce7908594bea75ec9d7e3dc429753" default))
  '(org-ditaa-jar-path
    "/nix/store/0g51hvz2286c7skks1hcykgr3s6fx2qq-ditaa-0.11.0/lib/ditaa.jar"))
 (custom-set-faces
